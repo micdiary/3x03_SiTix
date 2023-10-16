de1ef49b8c03689c36e64ba3fb29bc7dc6c66a48
Tennyson Koh <teenykoh@gmail.com>
2023-10-16 11:57:36 +0800

    WIP for UI Testing

---
+++ client/src/__tests__/App.test.js
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react';
+import App from 'client/src/App';
+
+test('renders App without crashing', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/home/i);
+  expect(linkElement).toBeInTheDocument();
+});
