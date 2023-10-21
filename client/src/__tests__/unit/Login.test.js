cf86ef22e2220e0cb9d99838836e296f44cf3979
Tennyson Koh <teenykoh@gmail.com>
2023-10-22 03:44:30 +0800

    Implemented working Unit Test

--- client/src/__tests__/unit/Login.test.js
+++ client/src/__tests__/unit/Login.test.js
@@ -1,33 +1,43 @@
 import { render, fireEvent, waitFor } from "@testing-library/react";
+import '@testing-library/jest-dom';
 import Login from 'client/src/pages/Login';
 import { BrowserRouter } from "react-router-dom";
+import React from "react";
+
+//... rest of your code.
 
 describe('Login', () => {
   it('renders correctly', () => {
-    const { getByText, getByPlaceholderText } = render(<BrowserRouter><Login /></BrowserRouter>);
+    const { getAllByText, getByPlaceholderText } = render(<BrowserRouter><Login /></BrowserRouter>);
     
-    expect(getByText('Login')).toBeInTheDocument();
+    const loginElements = getAllByText('Login');
+    expect(loginElements[0]).toBeInTheDocument();
     expect(getByPlaceholderText('Username')).toBeInTheDocument();
     expect(getByPlaceholderText('Password')).toBeInTheDocument();
   });
 
   it('shows an error if the form is submitted without a username', async () => {
-    const { getByText, getByRole } = render(<BrowserRouter><Login /></BrowserRouter>);
+    const { getByRole, getAllByRole } = render(<BrowserRouter><Login /></BrowserRouter>);
     
-    fireEvent.click(getByText('Login'));
+    fireEvent.click(getByRole('button', { name: /login button/i }));
     
     await waitFor(() => {
-      expect(getByRole('alert')).toHaveTextContent('Please enter your Username.');
+      const alerts = getAllByRole('alert');
+      // find the specific alert you expect based on its text content
+      const usernameAlert = alerts.find(alert => alert.textContent === 'Please enter your Username.');
+      expect(usernameAlert).toBeInTheDocument();
     });
   });
 
   it('shows an error if the form is submitted without a password', async () => {
-    const { getByText, getByRole } = render(<BrowserRouter><Login /></BrowserRouter>);
+    const { getByText, getByRole, getAllByRole } = render(<BrowserRouter><Login /></BrowserRouter>);
     
-    fireEvent.click(getByText('Login'));
+    fireEvent.click(getByRole('button', { name: /login button/i }));
     
     await waitFor(() => {
-      expect(getByRole('alert')).toHaveTextContent('Please enter your Password.');
+      const alerts = getAllByRole('alert');
+      // let's assume the password alert is the second one
+      expect(alerts[1]).toHaveTextContent('Please enter your Password.');
     });
   });
 });
