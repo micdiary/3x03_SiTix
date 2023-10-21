fa603a91d5edbd530f4a00a4932abc2fbda680b4
Tennyson Koh <teenykoh@gmail.com>
2023-10-22 03:25:33 +0800

    Implemented working Ui Test
    YAY it works:)

--- client/src/__tests__/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react';
-import App from 'client/src/App';
-
-test('renders App without crashing', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/home/i);
-  expect(linkElement).toBeInTheDocument();
-});
