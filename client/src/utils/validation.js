a16753126feb70f566238a7e0054439e1482d609
suqinn <93350856+suqinn@users.noreply.github.com>
2023-10-25 14:50:40 +0800

    Update validation.js
    change minimum length of password to suit report

--- client/src/utils/validation.js
+++ client/src/utils/validation.js
@@ -21,12 +21,14 @@ export const validateExpiryDate = (value) => {
 export function getPasswordValidationRule(customRules = []) {
     const passwordRules = [
         {
-            min: 8,
-            message: "Password must contain at least 8 characters!",
+            //min: 8,
+            min: 10,
+            message: "Password must contain at least 10 characters!",
         },
         {
             pattern:
-                /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+                ///^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+                /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{10,}$/,
             message:
                 "Password must contain at least a number, an uppercase, a lowercase, and a special character!",
         },
