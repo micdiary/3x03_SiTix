967f4e8b5993f92601b71756b41eaca0bdfa5553
Sy989 <2103137@sit.singaporetech.edu.sg>
2023-10-28 15:59:35 +0800

    Update index.js
    headers: false

--- server/src/index.js
+++ server/src/index.js
@@ -67,6 +67,7 @@ const limiter = rateLimit({
   windowMs: 60 * 1000, // 1 minute
   max: 5, // limit each IP to 5 request per minute
   message: "Too many requests, please try again later.",
+  headers: false,
 });
 
 server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
