b164fa765aaaf5b1b2fe36c610e4371e1f8af9bd
Sy989 <2103137@sit.singaporetech.edu.sg>
2023-10-28 15:51:16 +0800

    Update index.js
    added import rate-limiting

--- server/src/index.js
+++ server/src/index.js
@@ -14,6 +14,8 @@ import { eventRouter } from "./routes/event.js";
 import { requestRouter } from "./routes/request.js";
 import { orderRouter } from "./routes/order.js";
 
+import rateLimit from "express-rate-limit";
+
 const app = express();
 
 app.use(express.json());
@@ -24,7 +26,7 @@ const uploadDir = "uploads/";
 
 // Create uploads folder if it doesn't exist
 if (!fs.existsSync(uploadDir)) {
-	fs.mkdirSync(uploadDir);
+  fs.mkdirSync(uploadDir);
 }
 
 // Routes
@@ -37,27 +39,27 @@ app.use("/request", requestRouter);
 app.use("/order", orderRouter);
 
 mysql_connection.connect((err) => {
-	if (err) console.error("Error connecting to the database:", err);
-	console.log("MySQL Connected!");
+  if (err) console.error("Error connecting to the database:", err);
+  console.log("MySQL Connected!");
 });
 
 redis_connection.connect(
-	console.log("Redis Connected on redis://www.busy-Shannon.cloud:8080!")
+  console.log("Redis Connected on redis://www.busy-Shannon.cloud:8080!")
 );
 
 const server = https.createServer(app);
 
 app.get("/set-cookie", (req, res) => {
-	res.cookie("name", "value", {
-		secure: true, // set to true if your using https
-		httpOnly: true,
-		// This attribute can help prevent cross-site request forgery (CSRF) attacks. In many cases, it's beneficial to set this attribute to "Strict."
-		sameSite: "Strict", // None, Lax, or Strict
-		path: "/", // specify cookie path
-		expires: new Date(Date.now() + 8 * 3600000), // cookie will be removed after 8 hours
-	});
+  res.cookie("name", "value", {
+    secure: true, // set to true if your using https
+    httpOnly: true,
+    // This attribute can help prevent cross-site request forgery (CSRF) attacks. In many cases, it's beneficial to set this attribute to "Strict."
+    sameSite: "Strict", // None, Lax, or Strict
+    path: "/", // specify cookie path
+    expires: new Date(Date.now() + 8 * 3600000), // cookie will be removed after 8 hours
+  });
 
-	res.send("Cookie is set");
+  res.send("Cookie is set");
 });
 
 server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
