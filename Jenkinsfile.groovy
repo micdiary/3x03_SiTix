afb2d62a3f9fec0b21c198074ad7fd3f5bfb6b96
“TeeNyKoH” <teenykoh@gmail.com>
2023-11-01 17:47:20 +0800

    Refactor Jenkinsfile: install dependencies before running tests
    - Move the "Install dependencies" stage to the beginning of the pipeline
    - Add a new stage called "Test" after the "OWASP Dependency-Check Vulnerabilities" stage
    - In the "Test" stage, run tests using npm and generate JUnit reports

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,39 +2,31 @@ pipeline {
     agent any
 
     stages {
+        
         stage('Install dependencies') {
             steps {
                 echo 'Installing dependencies...'
                 sh 'npm ci'
             }
         }
+        stage('OWASP Dependency-Check Vulnerabilities') {
+            steps { 
+                dependencyCheck additionalArguments: ''' 
+                    -o './'
+                    -s './'
+                    -f 'ALL' 
+                    --prettyPrint''', odcInstallation: 'OWASP Dependency-Check Vulnerabilities'
+			dependencyCheckPublisher pattern: 'dependency-check-report.xml'
+                    }
+        }
         stage('Test') {
             steps {
                 echo 'Running tests...'
                 sh 'npm test'
             }
-            post {
-                always {
-                    junit '**/junit.xml'
-                }
-            } 
         }
-        stage('OWASP Dependency-Check Vulnerabilities') {
-            steps { 
-                dependencyCheck additionalArguments: ''' 
-                    --suppression "yarn.json"
-                    -o './'
-                    -s './'
-                    -f 'ALL' 
-                    --prettyPrint''', odcInstallation: 'OWASP Dependency-Check Vulnerabilities'
-                    }
-                    post {
-		    success {
-			dependencyCheckPublisher pattern: 'dependency-check-report.xml'
-		}
-	}
                 }
     }
-}
+
 
 
