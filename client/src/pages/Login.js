67c2a568f83657530cdbffb8f4b1b36f6b7d499f
jiaxinlarhh <mandyjiaxin98@gmail.com>
2023-11-03 23:12:06 +0800

    update login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { login } from "../api/account";
 import * as constants from "../constants";
 import Buttons from "../components/Buttons";
@@ -6,138 +6,196 @@ import { useNavigate } from "react-router-dom";
 import { LockOutlined, UserOutlined } from "@ant-design/icons";
 import { Col, Row, Form, Input, Typography, Card } from "antd";
 import {
-  aStyle,
-  cardStyle,
-  inputStyle,
-  marginBottomOneStyle,
-  typographyStyle,
+    aStyle,
+    cardStyle,
+    inputStyle,
+    marginBottomOneStyle,
+    typographyStyle,
 } from "./PagesStyles";
 import { userStore } from "../store/User";
+import Modals from "../components/Modal";
 
 const Login = () => {
-  let navigate = useNavigate();
-  const setStoreToken = userStore((state) => state.setToken);
-  const setStoreUserType = userStore((state) => state.setUserType);
+    let navigate = useNavigate();
+    const setStoreToken = userStore((state) => state.setToken);
+    const setStoreUserType = userStore((state) => state.setUserType);
+    const [modalVisible, setModalVisible] = useState(false);
+    const [form] = Form.useForm();
 
-  const onFinish = (values) => {
+    const onFinish = (values) => {
+        const req = {
+            username: values.username,
+            password: values.password,
+        };
 
-    const req = {
-      username: values.username,
-      password: values.password,
+        login(req)
+            .then((res) => {
+                console.log(res);
+                // global state
+                setStoreToken(res.token);
+                setStoreUserType(res.userType);
+
+                // local storage
+                localStorage.setItem("token", res.token);
+                localStorage.setItem("userType", res.userType);
+
+                // TODO change accordingly depending on usertype
+                if (res.userType === "superadmin") {
+                    navigate(constants.SUPERADMIN_URL);
+                } else if (res.userType === "admin") {
+                    navigate(constants.ADMIN_URL);
+                } else {
+                    navigate(constants.HOME_URL);
+                }
+            })
+            .catch((err) => {
+                alert(err.message);
+            });
     };
 
-    login(req)
-      .then((res) => {
+    const submitOTP = () => {
+        //
+    };
 
-        // global state
-        setStoreToken(res.token);
-        setStoreUserType(res.userType);
+    const handleModalOk = () => {
+        form.submit();
+    };
 
-        // local storage
-        localStorage.setItem("token", res.token);
-        localStorage.setItem("userType", res.userType);
+    const handleModalCancel = () => {
+        setModalVisible(false);
+        form.resetFields();
+    };
 
-        // TODO change accordingly depending on usertype
-        if (res.userType === "superadmin") {
-          navigate(constants.SUPERADMIN_URL);
-        } else if (res.userType === "admin") {
-          navigate(constants.ADMIN_URL);
-        } else {
-          navigate(constants.HOME_URL);
-        }
-      })
-      .catch((err) => {
-        alert(err.message);
-      });
-  };
-
-  return (
-    <Row
-      justify="center"
-      align="middle"
-      style={{
-        minHeight: "100vh",
-        background: `url(https://pngimg.com/d/smoke_PNG55185.png) no-repeat center`,
-      }}
-    >
-      <Col xs={20} sm={16} md={12} lg={8}>
-        <Typography.Title style={typographyStyle} level={1}>
-          Login
-        </Typography.Title>
-        <Card style={cardStyle}>
-          <Form
-            layout="vertical"
-            name="normal_login"
-            initialValues={{ remember: true }}
-            onFinish={onFinish}
-          >
+    const modalForm = (formItems) => {
+        return formItems.map((item, index) => (
             <Form.Item
-              name="username"
-              rules={[
-                {
-                  required: true,
-                  message: "Please enter your Username.",
-                  type: "string",
-                },
-                {
-                  max: 50,
-                  message: "Username can be at most 50 characters long.",
-                  //Extremely long usernames can potentially be used to perform denial-of-service attacks, either unintentionally or intentionally
-                  //Prevent consuming significant server resources or leads to application instability.
-                },
-              ]}
-              style={marginBottomOneStyle}
+                name={item.name}
+                rules={item.rules}
+                key={index}
+                valuePropName={item.valuePropName}
+                style={marginBottomOneStyle}
             >
-              <Input
-                style={inputStyle}
-                prefix={<UserOutlined />}
-                placeholder="Username"
-              />
+                {item.input}
             </Form.Item>
-            <Form.Item
-              name="password"
-              rules={[
+        ));
+    };
+
+    const formItems = [
+        {
+            name: "loginOTP",
+            rules: [
                 {
-                  required: true,
-                  message: "Please enter your Password.",
+                    required: true,
+                    message: "Required",
                 },
-                {
-                  max: 50,
-                  message: "Password can be at most 50 characters long.",
-                  //Extremely long characters can potentially be used to perform denial-of-service attacks, either unintentionally or intentionally
-                  //Prevent consuming significant server resources or leads to application instability.
-                },
-              ]}
-            >
-              <Input.Password
-                style={inputStyle}
-                prefix={<LockOutlined />}
-                placeholder="Password"
-              />
-            </Form.Item>
-            <Form.Item
-              style={{
-                marginTop: "-20px",
-                display: "flex",
-                justifyContent: "right",
-              }}
-            >
-              <a style={aStyle} href={constants.FORGET_PASSWORD_URL}>
-                Forgot Password?
-              </a>
-            </Form.Item>
-            <Form.Item>
-              <Buttons arialLabel="login button" text="Login" />
-              Or{" "}
-              <a style={aStyle} href={constants.REGISTER_URL}>
-                register now!
-              </a>
-            </Form.Item>
-          </Form>
-        </Card>
-      </Col>
-    </Row>
-  );
+            ],
+            input: <Input style={inputStyle} placeholder="OTP" />,
+        },
+    ];
+
+    return (
+        <Row
+            justify="center"
+            align="middle"
+            style={{
+                minHeight: "100vh",
+                background: `url(https://pngimg.com/d/smoke_PNG55185.png) no-repeat center`,
+            }}
+        >
+            <Col xs={20} sm={16} md={12} lg={8}>
+                <Typography.Title style={typographyStyle} level={1}>
+                    Login
+                </Typography.Title>
+                <Card style={cardStyle}>
+                    <Form
+                        layout="vertical"
+                        name="normal_login"
+                        initialValues={{ remember: true }}
+                        onFinish={onFinish}
+                    >
+                        <Form.Item
+                            name="username"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: "Please enter your Username.",
+                                    type: "string",
+                                },
+                                {
+                                    max: 50,
+                                    message:
+                                        "Username can be at most 50 characters long.",
+                                    //Extremely long usernames can potentially be used to perform denial-of-service attacks, either unintentionally or intentionally
+                                    //Prevent consuming significant server resources or leads to application instability.
+                                },
+                            ]}
+                            style={marginBottomOneStyle}
+                        >
+                            <Input
+                                style={inputStyle}
+                                prefix={<UserOutlined />}
+                                placeholder="Username"
+                            />
+                        </Form.Item>
+                        <Form.Item
+                            name="password"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: "Please enter your Password.",
+                                },
+                                {
+                                    max: 50,
+                                    message:
+                                        "Password can be at most 50 characters long.",
+                                    //Extremely long characters can potentially be used to perform denial-of-service attacks, either unintentionally or intentionally
+                                    //Prevent consuming significant server resources or leads to application instability.
+                                },
+                            ]}
+                        >
+                            <Input.Password
+                                style={inputStyle}
+                                prefix={<LockOutlined />}
+                                placeholder="Password"
+                            />
+                        </Form.Item>
+                        <Form.Item
+                            style={{
+                                marginTop: "-20px",
+                                display: "flex",
+                                justifyContent: "right",
+                            }}
+                        >
+                            <a
+                                style={aStyle}
+                                href={constants.FORGET_PASSWORD_URL}
+                            >
+                                Forgot Password?
+                            </a>
+                        </Form.Item>
+                        <Form.Item>
+                            <Buttons arialLabel="login button" text="Login" />
+                            Or{" "}
+                            <a style={aStyle} href={constants.REGISTER_URL}>
+                                register now!
+                            </a>
+                        </Form.Item>
+                    </Form>
+                </Card>
+                <Modals
+                    modal2Open={modalVisible}
+                    closeModal={handleModalCancel}
+                    modalTitle={"OTP"}
+                    modalContent={
+                        <Form form={form} onFinish={submitOTP}>
+                            {modalForm(formItems)}
+                        </Form>
+                    }
+                    onOk={handleModalOk}
+                />
+            </Col>
+        </Row>
+    );
 };
 
 export default Login;
